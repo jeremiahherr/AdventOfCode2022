54a8f3d50df190f5e10efccd51e67c4ed6f426ed
Jeremiah Allen Herr <jaherr77@gmail.com>
2022-12-10 16:53:35 -0600

    Solve Day 3 Part 1

--- adventOfCode/day3/ElfRucksackReorganizer.java
+++ adventOfCode/day3/ElfRucksackReorganizer.java
@@ -1,13 +1,109 @@
 
 package adventOfCode.day3;
 
+import java.util.HashMap;
 import adventOfCode.utils.*;
 
 public class ElfRucksackReorganizer 
 {
-    public static int getRucksackPrioritySum(String ruckSacks)
+    public static int getRucksackPrioritySum(String rucksacks)
     {
-        return 0;
+        var rucksackPrioritySum = 0;
+        for (var rucksack : getRucksacks(rucksacks))
+        {
+            rucksackPrioritySum += getItemPriority(getItemSharedByBothRucksackCompartments(getRucksackCompartments(rucksack)));
+        }
+        return rucksackPrioritySum;
+    }
+
+    private static String[] getRucksacks(String rucksacksList)
+    {
+        var rucksacks = rucksacksList.split(Settings.NEW_LINE);
+        return rucksacks;
+    }
+
+    private static String[] getRucksackCompartments(String rucksack)
+    {
+        var rucksackCompartments = new String[2];
+        int middleItemIndex = rucksack.length() / 2;
+        rucksackCompartments[0] = rucksack.substring(0, middleItemIndex);
+        rucksackCompartments[1] = rucksack.substring(middleItemIndex);
+        return rucksackCompartments;
+    }
+
+    private static char getItemSharedByBothRucksackCompartments(String[] rucksackCompartments)
+    {
+        var compartment1 = rucksackCompartments[0];
+        var compartment2 = rucksackCompartments[1];
+        for (int item = 0; item < compartment1.length(); item++)
+        {
+            var currentItem = compartment1.charAt(item);
+            if (compartment2.contains(String.valueOf(currentItem))) return currentItem;
+        }
+        return Character.MIN_VALUE;
+    }
+
+    private static HashMap<String, Integer> getItemPriorityDictionary()
+    {
+        var itemPriorityDictionary = new HashMap<String, Integer>();
+        itemPriorityDictionary.put("a", 1);
+        itemPriorityDictionary.put("b", 2);
+        itemPriorityDictionary.put("c", 3);
+        itemPriorityDictionary.put("d", 4);
+        itemPriorityDictionary.put("e", 5);
+        itemPriorityDictionary.put("f", 6);
+        itemPriorityDictionary.put("g", 7);
+        itemPriorityDictionary.put("h", 8);
+        itemPriorityDictionary.put("i", 9);
+        itemPriorityDictionary.put("j", 10);
+        itemPriorityDictionary.put("k", 11);
+        itemPriorityDictionary.put("l", 12);
+        itemPriorityDictionary.put("m", 13);
+        itemPriorityDictionary.put("n", 14);
+        itemPriorityDictionary.put("o", 15);
+        itemPriorityDictionary.put("p", 16);
+        itemPriorityDictionary.put("q", 17);
+        itemPriorityDictionary.put("r", 18);
+        itemPriorityDictionary.put("s", 19);
+        itemPriorityDictionary.put("t", 20);
+        itemPriorityDictionary.put("u", 21);
+        itemPriorityDictionary.put("v", 22);
+        itemPriorityDictionary.put("w", 23);
+        itemPriorityDictionary.put("x", 24);
+        itemPriorityDictionary.put("y", 25);
+        itemPriorityDictionary.put("z", 26);
+        itemPriorityDictionary.put("A", 27);
+        itemPriorityDictionary.put("B", 28);
+        itemPriorityDictionary.put("C", 29);
+        itemPriorityDictionary.put("D", 30);
+        itemPriorityDictionary.put("E", 31);
+        itemPriorityDictionary.put("F", 32);
+        itemPriorityDictionary.put("G", 33);
+        itemPriorityDictionary.put("H", 34);
+        itemPriorityDictionary.put("I", 35);
+        itemPriorityDictionary.put("J", 36);
+        itemPriorityDictionary.put("K", 37);
+        itemPriorityDictionary.put("L", 38);
+        itemPriorityDictionary.put("M", 39);
+        itemPriorityDictionary.put("N", 40);
+        itemPriorityDictionary.put("O", 41);
+        itemPriorityDictionary.put("P", 42);
+        itemPriorityDictionary.put("Q", 43);
+        itemPriorityDictionary.put("R", 44);
+        itemPriorityDictionary.put("S", 45);
+        itemPriorityDictionary.put("T", 46);
+        itemPriorityDictionary.put("U", 47);
+        itemPriorityDictionary.put("V", 48);
+        itemPriorityDictionary.put("W", 49);
+        itemPriorityDictionary.put("X", 50);
+        itemPriorityDictionary.put("Y", 51);
+        itemPriorityDictionary.put("Z", 52);
+        return itemPriorityDictionary;
+    }
+
+    private static int getItemPriority(char item)
+    {
+        return getItemPriorityDictionary().get(String.valueOf(item));
     }
 
     public static void main(String[] args)
