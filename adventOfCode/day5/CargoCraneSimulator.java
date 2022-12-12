56eaac5e0438b66f2644e96f492bb45f796bf16f
Jeremiah Allen Herr <jaherr77@gmail.com>
2022-12-12 00:29:19 -0600

    Solve Day 5 Part 1

--- adventOfCode/day5/CargoCraneSimulator.java
+++ adventOfCode/day5/CargoCraneSimulator.java
@@ -1,12 +1,96 @@
+
 package adventOfCode.day5;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Stack;
 import adventOfCode.utils.*;
 
 public class CargoCraneSimulator 
 {
     public static String getFinalCrateArrangement(String crateRearrangementInstructions)
     {
-        return Settings.EMPTY_STRING;
+        var initialArrangementAndInstructions = separateInitialArrangementAndMoveInstructions(crateRearrangementInstructions);
+        var crateStacks = getInitialCrateArrangement(initialArrangementAndInstructions[0]); // not getting correct initial arrangement of stacks
+        var moveInstructions = initialArrangementAndInstructions[1];
+        for (var instructionLine : moveInstructions.split(Settings.NEW_LINE))
+        {
+            crateStacks = carryOutInstructionLine(instructionLine, crateStacks);
+        }
+        return getCrateAtTopOfEachStack(crateStacks);
+    }
+
+    private static String[] separateInitialArrangementAndMoveInstructions(String crateRearrangementInstructions)
+    {
+        var initialArrangementAndInstructions = new String[2];
+        var dividingIndex = crateRearrangementInstructions.indexOf("move");
+        initialArrangementAndInstructions[0] = crateRearrangementInstructions.substring(0, dividingIndex); // Initial arrangement of crate stacks
+        initialArrangementAndInstructions[1] = crateRearrangementInstructions.substring(dividingIndex); // Crate move instructions
+        return initialArrangementAndInstructions;
+    }
+
+    private static ArrayList<Stack<String>> getInitialCrateArrangement(String initialCrateArrangement)
+    {
+        var crateArrangementLines = initialCrateArrangement.split(Settings.NEW_LINE);
+        var numberOfCrateStacks = crateArrangementLines[crateArrangementLines.length - 1].split("   ").length;
+        var crateStacks = new ArrayList<Stack<String>>(numberOfCrateStacks);
+        for (int stack = 0; stack < numberOfCrateStacks; stack++)
+        {
+            crateStacks.add(new Stack<String>());
+        }
+        var cratePositionMap = mapCratePositionInLineToStackIndex(numberOfCrateStacks);
+
+        for (int lineIndex = crateArrangementLines.length - 2; lineIndex >= 0; lineIndex--)
+        {
+            var line = crateArrangementLines[lineIndex];
+            for (int characterIndex = 1; characterIndex <= line.length(); characterIndex += 4)
+            {
+                var character = String.valueOf(line.charAt(characterIndex));
+                var characterIsNotWhitespace = !character.isEmpty() && !character.trim().isEmpty();
+                if (characterIsNotWhitespace)
+                {
+                    var stackIndex = cratePositionMap.get(characterIndex);
+                    crateStacks.get(stackIndex).push(character);
+                }
+            }
+        }
+        return crateStacks;
+    }
+
+    private static HashMap<Integer, Integer> mapCratePositionInLineToStackIndex(int numberOfCrateStacks)
+    {
+        var cratePositionInLineToStackIndexMap = new HashMap<Integer, Integer>();
+        var cratePositionInLine = 1;
+        for (int stackIndex = 0; stackIndex < numberOfCrateStacks; stackIndex++)
+        {
+            cratePositionInLineToStackIndexMap.put(cratePositionInLine, stackIndex);
+            cratePositionInLine += 4;
+        }
+        return cratePositionInLineToStackIndexMap;
+    }
+
+    private static ArrayList<Stack<String>> carryOutInstructionLine(String instructionLine, ArrayList<Stack<String>> crateStacks)
+    {
+        String[] instruction = instructionLine.split(Settings.EMPTY_SPACE);
+        int cratesToMove = Integer.parseInt(instruction[1]);
+        int fromStack = Integer.parseInt(instruction[3])-1;
+        int toStack = Integer.parseInt(instruction[5])-1;
+
+        for (int crate = 0; crate < cratesToMove; crate++)
+        {
+            crateStacks.get(toStack).push(crateStacks.get(fromStack).pop());
+        }
+        return crateStacks;
+    }
+
+    private static String getCrateAtTopOfEachStack(ArrayList<Stack<String>> crateStacks)
+    {
+        var crateMessage = Settings.EMPTY_STRING;
+        for (var stack : crateStacks)
+        {
+            crateMessage += stack.peek();
+        }
+        return crateMessage;
     }
 
     public static void main(String[] args)
